276b23b6d0683b4fa77b5b78bdaf62f82beffdd3
nheron <nicolas.heron@proton.me>
2025-05-24 15:21:06 +0200

    Version apache-kie-10

---
+++ src/test/java/droolscours/Testlesson4.java
@@ -0,0 +1,47 @@
+package droolscours;
+
+import droolscours.util.OutputDisplay;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import util.KnowledgeSessionHelper;
+
+
+public class Testlesson4 {
+	static KieContainer kieContainer;
+	KieSession sessionStatefull = null;
+
+	@BeforeClass
+	public static void beforeClass() {
+		kieContainer = KnowledgeSessionHelper.createRuleBase();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("------------Before------------");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("------------After ------------");
+	}
+	
+	
+
+	@Test
+	public void testRuleFlow1() {
+		sessionStatefull = KnowledgeSessionHelper
+				.getStatefulKnowledgeSessionForJBPM(kieContainer, "lesson4-session");
+		OutputDisplay display = new OutputDisplay();
+		sessionStatefull.setGlobal("showResult", display);
+		Account a = new Account();
+		sessionStatefull.insert(a);
+		sessionStatefull.startProcess("RF1");
+		sessionStatefull.fireAllRules();
+	}
+
+
+}
